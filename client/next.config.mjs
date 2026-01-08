aa882cee635087bed956f3d7af2002f57bc8b384
Elumugam <elumugamelumugam@gmail.com>
2026-01-08 19:34:41 +0700

    Fix: Move netlify.toml to root and enable image unoptimization for static export

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com', 'i.ytimg.com'],
+        unoptimized: true,
     },
     eslint: {
         ignoreDuringBuilds: true,
