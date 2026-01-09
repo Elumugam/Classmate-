88a2f7c348104ab3c7c81dc3870fde3b06cbc9b2
Elumugam <elumugamelumugam@gmail.com>
2026-01-09 14:51:39 +0700

    build: ignore eslint and typescript errors during production build

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: "export",
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
 };
+
+module.exports = nextConfig;
